d2f6962cd0619b0e7aeeb2b8c3291ad92e5ad403
Augus <145805991+au6us@users.noreply.github.com>
2025-09-27 20:22:05 -0300

    FOG HECHO

--- Assets/Scripts/Cameras/AisleCamera.cs
+++ Assets/Scripts/Cameras/AisleCamera.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 public class AisleCamera : MonoBehaviour
 {
     [SerializeField] private CinemachineFreeLook myVirtualCamera;
-    [SerializeField] private CinemachineVirtualCameraBase myVirtualCameraBase;
+    //[SerializeField] private CinemachineVirtualCameraBase myVirtualCameraBase;
 
     private void Start()
     {
         myVirtualCamera.gameObject.SetActive(false);
-        myVirtualCameraBase.gameObject.SetActive(false);
+        //myVirtualCameraBase.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         myVirtualCamera.gameObject.SetActive(true);
-        myVirtualCameraBase.gameObject.SetActive(true);
+        //myVirtualCameraBase.gameObject.SetActive(true);
 
     }
 
     private void OnTriggerExit(Collider other)
     {
         myVirtualCamera.gameObject.SetActive(false);
-        myVirtualCameraBase.gameObject.SetActive(false);
+        //myVirtualCameraBase.gameObject.SetActive(false);
     }
 
 }
