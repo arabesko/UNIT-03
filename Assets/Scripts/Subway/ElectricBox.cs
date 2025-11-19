a96ccd674c2b6c0d4c8eab438ca48a24027da9fa
Jxakx <lopezjoaquin488@gmail.com>
2025-11-18 23:31:34 -0300

    particulaass

--- Assets/Scripts/Subway/ElectricBox.cs
+++ Assets/Scripts/Subway/ElectricBox.cs
@@ -5,6 +5,9 @@ public class ElectricBox : MonoBehaviour
     [Header("Referencias de la Trampa")]
     [SerializeField] private GameObject deathZone; // Referencia al trigger de muerte
     [SerializeField] private ParticleSystem electricParticles; // Referencia al sistema de partículas
+    [SerializeField] private ParticleSystem electricParticles2;
+    [SerializeField] private ParticleSystem electricParticles3;
+    [SerializeField] private ParticleSystem electricParticles4;
     //[SerializeField] private Renderer boxRenderer; // Opcional: para cambiar el material
 
     //[Header("Configuración")]
@@ -19,6 +22,18 @@ public class ElectricBox : MonoBehaviour
         // Desactivar partículas
         if (electricParticles != null)
             electricParticles.Stop();
+
+        // Desactivar partículas
+        if (electricParticles2 != null)
+            electricParticles2.Stop();
+
+        // Desactivar partículas
+        if (electricParticles3 != null)
+            electricParticles3.Stop();
+
+        // Desactivar partículas
+        if (electricParticles4 != null)
+            electricParticles4.Stop();
 
         // Opcional: cambiar apariencia de la caja
         /*if (boxRenderer != null && damagedMaterial != null)
